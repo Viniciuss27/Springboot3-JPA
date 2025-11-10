e2fc137be78f189be90df907e38bae936b01fa35
Vinicius de Jesus <viniciusdejjesuspessoal@gmail.com>
2025-11-10 07:46:13 -0300

    repositorioJpa, BD config

---
+++ src/main/java/com/educandoweb/curso/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.educandoweb.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.curso.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	//conecta o User a o repositorio Jpa
+
+}
