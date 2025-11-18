f8906c88fcd11e96b61c6ac261457a7084d41c59
Vinicius de Jesus <viniciusdejjesuspessoal@gmail.com>
2025-11-18 07:03:38 -0300

    Entidade OrdeItem

--- src/main/java/com/educandoweb/curso/entities/Order.java
+++ src/main/java/com/educandoweb/curso/entities/Order.java
@@ -2,6 +2,8 @@ package com.educandoweb.curso.entities;//"" ''
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.educandoweb.curso.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -12,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,7 +26,7 @@ public class Order implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd''T'HH:mm:ss'Z'", timezone = "GMT")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
 	private Integer orderStatus;
@@ -31,6 +34,9 @@ public class Order implements Serializable{
 	@ManyToOne //chave extrangeira
 	@JoinColumn(name = "client_id") //nome da chave extrangeira
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order") //um para muitos
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Order() {}
 
@@ -74,6 +80,10 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public Set<OrderItem> getItems(){
+		return items;
 	}
 
 	@Override
---
+++ src/main/java/com/educandoweb/curso/entities/pk/OrderItemPK.java
@@ -0,0 +1,68 @@
+package com.educandoweb.curso.entities.pk;
+
+import java.io.Serializable;
+
+import com.educandoweb.curso.entities.Order;
+import com.educandoweb.curso.entities.Product;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable //classe auxiliar, chave primaria composta
+public class OrderItemPK implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne // muitos para um
+	@JoinColumn(name = "order_id")//nome da coluna da chave extrangeira
+	private Order order;
+	
+	@ManyToOne // muitos para um
+	@JoinColumn(name = "product_id")//nome da coluna da chave extrangeira
+	private Product product;
+	
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((order == null) ? 0 : order.hashCode());
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPK other = (OrderItemPK) obj;
+		if (order == null) {
+			if (other.order != null)
+				return false;
+		} else if (!order.equals(other.order))
+			return false;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		return true;
+	}
+	
+	
+}
