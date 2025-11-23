a9b2233d18f9d8df2af51eab40ab5eb631e4571b
Vinicius de Jesus <viniciusdejjesuspessoal@gmail.com>
2025-11-23 09:29:47 -0300

    tratando excecao do findById

---
+++ src/main/java/com/educandoweb/curso/service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.educandoweb.curso.service.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource Not Found Id " + id);
+	}
+
+}
