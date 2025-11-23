8280204d46e3d78dc548d93241eda0e0a4fdd7d9
Vinicius de Jesus <viniciusdejjesuspessoal@gmail.com>
2025-11-23 11:13:05 -0300

    tratando excecao de update

--- src/main/java/com/educandoweb/curso/service/UserService.java
+++ src/main/java/com/educandoweb/curso/service/UserService.java
@@ -13,6 +13,8 @@ import com.educandoweb.curso.repositories.UserRepository;
 import com.educandoweb.curso.service.exceptions.DatabaseException;
 import com.educandoweb.curso.service.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,14 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);// monitora objeto do banco de dados
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new DatabaseException(e.getMessage());
+
+		}
 
 	}
 
